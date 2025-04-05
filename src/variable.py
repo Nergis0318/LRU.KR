cff1c37a64f013d816abdefdf517132ee487c2a8
Nergis <me@nergis.dev>
2025-04-06 04:08:18 +0900

    Changes

--- src/variable.py
+++ src/variable.py
@@ -10,9 +10,6 @@ emoji_list = list()
 for _ in emoji_data:
     emoji_list.append(_[0])
 
-DOMAIN = dotenv.get_key(".env", "DOMAIN")
-DB = dotenv.get_key(".env", "DB")
-KEY_DB = dotenv.get_key(".env", "KEY_DB")
-DONATE_DB = dotenv.get_key(".env", "DONATE_DB")
-PEPPER_DB = dotenv.get_key(".env", "PEPPER_DB")
-EMOJI_DB = dotenv.get_key(".env", "EMOJI_DB")
+DOMAIN = dotenv.get_key("/.env", "DOMAIN")
+DB = dotenv.get_key("/.env", "DB")
+EMOJI_DB = dotenv.get_key("/.env", "EMOJI_DB")
