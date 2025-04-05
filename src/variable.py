c1b807fa59960ee739e96f076a56e906060febd8
Nergis <me@nergis.dev>
2025-04-06 04:31:38 +0900

    Changes

--- src/variable.py
+++ src/variable.py
@@ -13,3 +13,4 @@ for _ in emoji_data:
 DOMAIN = dotenv.get_key("/.env", "DOMAIN")
 DB = dotenv.get_key("/.env", "DB")
 EMOJI_DB = dotenv.get_key("/.env", "EMOJI_DB")
+KEY_DB = dotenv.get_key("/.env", "KEY_DB")
